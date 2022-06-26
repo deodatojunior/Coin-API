635a9b4d4ffdcbebce361d1aad94d0513c53fd9a
Deodato Junior <deodatojunior999@gmail.com>
2022-06-25 22:42:22 -0400

    creating front-end

--- api/views.py
+++ api/views.py
@@ -1,22 +1,36 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 import requests as req
 link = 'https://economia.awesomeapi.com.br/json/'
 link_alt = 'https://economia.awesomeapi.com.br/'
 
 
+def initial_form(request):
+    if request.method == 'POST':
+        code = request.POST['code']
+        quantity = request.POST['quantity']
+        days = request.POST['days']
+        if code != "" and quantity != "":
+            return redirect('get_info_coin_by_code_and_quantity', coin=code, quantity=quantity)
+        elif code != "" and days != "":
+            return redirect('get_info_coin_by_code_and_days', coin=code, days=days)
+        elif code != "":
+            return redirect('get_info_coin_by_code_in_realtime', coin=code)
+    return render(request, 'base.html')
+
+
 def get_info_coin_by_code_and_quantity(request, coin, quantity):
     connect = req.get(link + f"{coin}/{quantity}")
     response = connect.json()
-    return HttpResponse(response)
+    return render(request, 'get_info_coin_by_code_and_quantity.html', {'list': response})
 
 
 def get_info_coin_by_code_and_days(request, coin, days):
     connect = req.get(link_alt + f"json/daily/{coin}/{days}")
     response = connect.json()
-    return HttpResponse(response)
+    return render(request, 'get_info_coin_by_code_and_days.html', {'list': response})
 
 
 def get_info_coin_by_code_in_realtime(request, coin):
     connect = req.get(link_alt + f"last/{coin}/")
     response = connect.json()
-    return HttpResponse(response.items())
+    return render(request, 'get_info_coin_by_code_in_realtime.html', {'list': response.items()})
--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,14 @@ import api.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', views.initial_form, name='initial_form'),
     path('api/v1/<str:coin>/<str:quantity>/', views.get_info_coin_by_code_and_quantity,
-         name="get_info_coin_with_quantity"),
+         name="get_info_coin_by_code_and_quantity"),
     path('api/v2/<str:coin>/<int:days>/',
          views.get_info_coin_by_code_and_days,
-         name="get_info_coin_by_days"),
+         name="get_info_coin_by_code_and_days"),
     path('api/v2/<str:coin>/',
          views.get_info_coin_by_code_in_realtime,
-         name="get_info_coin_in_realtime")
+         name="get_info_coin_by_code_in_realtime")
+
 ]
